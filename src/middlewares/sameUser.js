b67ce8067f04935716094754155e94dd5e8703c9
navid hero <navid_hero_1st@yahoo.com>
2024-06-24 12:26:59 +0330

    same user

--- src/middlewares/sameUser.js
+++ src/middlewares/sameUser.js
@@ -1,13 +1,16 @@
 const { loginWithAuthToken } = require("../controllers/Client/AuthController");
 
 exports.sameUser = async (req, res, next) => {
-	if (!req.session.user) {
+	if (!req.session.user && req.header("Access-Token")) {
 		req.session.user = await loginWithAuthToken(req.header("Access-Token"));
 	}
 
+	console.log(req.session.user);
+
 	if (
-		req.session.user._id === req.body.id ||
-		req.session.user._id === req.params.id
+		req.session.user &&
+		(req.session.user._id === req.body.id ||
+			req.session.user._id === req.params.id)
 	) {
 		next();
 	} else {
