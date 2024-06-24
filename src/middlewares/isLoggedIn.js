f22f825ab29eeee0713f323f31a105c27d9dc99e
navid hero <navid_hero_1st@yahoo.com>
2024-06-24 12:10:26 +0330

    req.header

--- src/middlewares/isLoggedIn.js
+++ src/middlewares/isLoggedIn.js
@@ -3,7 +3,7 @@ const { loginWithAuthToken } = require("../controllers/Client/AuthController");
 exports.isLoggedIn = async (req, res, next) => {
 	if (req.session.user) {
 		next();
-	} else if (req.headers["Access-Token"]) {
+	} else if (req.header("Access-Token")) {
 		req.session.user = await loginWithAuthToken(req.headers["Access-Token"]);
 		next();
 	} else {
