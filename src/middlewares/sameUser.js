ec2925fbd7f140a4f892fde462c6aad6fb31ba1a
navid hero <navid_hero_1st@yahoo.com>
2024-06-25 17:35:34 +0330

    test

--- src/middlewares/sameUser.js
+++ src/middlewares/sameUser.js
@@ -5,6 +5,8 @@ exports.sameUser = async (req, res, next) => {
 		req.session.user = await loginWithAuthToken(req.header("Access-Token"));
 	}
 
+	console.log(req.session.user);
+
 	if (
 		req.session.user &&
 		(req.session.user._id?.toString() === req.body.id ||
