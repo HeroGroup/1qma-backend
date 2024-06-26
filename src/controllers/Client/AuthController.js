4d01462bf6d8dcbb7ed880915532d6d8f51fea7a
navid hero <navid_hero_1st@yahoo.com>
2024-06-26 11:53:46 +0330

    session back

--- src/controllers/Client/AuthController.js
+++ src/controllers/Client/AuthController.js
@@ -365,11 +365,7 @@ exports.choosePreferedLanguage = async (params, sessionUser) => {
 		return fail("invalid user id", params);
 	}
 
-	if (
-		params.providerId &&
-		sessionUser &&
-		sessionUser.providerId !== params.providerId
-	) {
+	if (params.providerId && sessionUser?.providerId !== params.providerId) {
 		return fail("invalid provider id!");
 	}
 
