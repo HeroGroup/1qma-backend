43e0416dca67c9899f1fa82b4d17e9243b4d0607
navid hero <navid_hero_1st@yahoo.com>
2024-07-01 11:08:46 +0330

    one origin only

--- app.js
+++ app.js
@@ -75,13 +75,13 @@ const whiteList = [
 ];
 const corsOptions = {
 	credentials: true,
-	origin: function (origin, callback) {
+	origin: "http://localhost:4200" /*function (origin, callback) {
 		if (whiteList.indexOf(origin) !== -1) {
 			callback(null, true);
 		} else {
 			callback(new Error("Not allowed by CORS"));
 		}
-	},
+	},*/,
 };
 
 app.use(cors(corsOptions));
