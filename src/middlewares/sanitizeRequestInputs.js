cbd576c0ad67ff260eeaad1edb83dd4fb0b829dd
navid hero <navid_hero_1st@yahoo.com>
2024-07-03 11:05:46 +0330

    fix: sanitize all request inputs

--- src/middlewares/sanitizeRequestInputs.js
+++ src/middlewares/sanitizeRequestInputs.js
@@ -2,10 +2,28 @@ const { sanitize } = require("../helpers/validator");
 
 exports.sanitizeRequestInputs = (req, res, next) => {
 	// sanitize inputs
-	const { body: params } = req;
-	const paramNames = Object.keys(params);
-	paramNames.forEach((elm) => {
-		req.body[elm] = sanitize(params[elm]);
+	const {
+		body: bodyParameters,
+		params: paramsParameters,
+		query: queryParameters,
+	} = req;
+
+	// req.body
+	const bodyNames = Object.keys(bodyParameters);
+	bodyNames.forEach((elm) => {
+		req.body[elm] = sanitize(bodyParameters[elm]);
+	});
+
+	// req.params
+	const paramsNames = Object.keys(bodyParameters);
+	paramsNames.forEach((elm) => {
+		req.params[elm] = sanitize(bodyParameters[elm]);
+	});
+
+	// req.query
+	const queryNames = Object.keys(bodyParameters);
+	queryNames.forEach((elm) => {
+		req.query[elm] = sanitize(bodyParameters[elm]);
 	});
 
 	next();
