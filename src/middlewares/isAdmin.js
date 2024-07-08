4e7bcc0b9a9908f6e6403fa21bca1cceacceaf74
navid hero <navid_hero_1st@yahoo.com>
2024-07-08 09:59:01 +0330

    log

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,4 +1,5 @@
 exports.isAdmin = async (req, res, next) => {
+	console.log("isAdmin", req.session.user);
 	if (req.session.user?._id && req.session.user.userType === "admin") {
 		next();
 	} else {
