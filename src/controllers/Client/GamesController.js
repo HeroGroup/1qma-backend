ec801f7e35032f2b6583c9361f44fafa213f0d2b
navid hero <navid_hero_1st@yahoo.com>
2024-07-28 12:48:34 +0330

    change

--- src/controllers/Client/GamesController.js
+++ src/controllers/Client/GamesController.js
@@ -36,8 +36,8 @@ exports.scoreboard = async (userId, page = 1, limit = 5) => {
 
 		const scoreboard = myGames.map((myGame) => {
 			const myRankIndex = myGame.result.scoreboard.findIndex((elm) => {
-				console.log(elm._id, objectId(userId));
-				return elm._id === objectId(userId);
+				console.log(elm._id.toString(), userId.toString());
+				return elm._id.toString() === userId.toString();
 			});
 
 			return {
