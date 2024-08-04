4ea736d0c9bb6a23496c6e34d250ef2dd652e269
navid hero <navid_hero_1st@yahoo.com>
2024-08-04 10:51:57 +0330

    fix: games

--- src/controllers/Client/GamesController.js
+++ src/controllers/Client/GamesController.js
@@ -8,10 +8,10 @@ exports.games = async (userId, type, category, page = 1, limit = 5) => {
 		const games = await Game.find(
 			{
 				status: "ended",
-				...(type === "all"
-					? {}
-					: { players: { _id: userId, status: "connected" } }),
-				...(category ? { "category.name": category } : {}),
+				...(type === "private"
+					? { "result.scoreboard._id": objectId(userId) }
+					: {}),
+				...(category ? { "category.id": category } : {}),
 			},
 			{ _id: 1, code: 1, creator: 1, category: 1, players: 1, gameType: 1 }
 		)
