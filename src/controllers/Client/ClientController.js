d9a3724ccea556b70a7b7025bcbba43a3c1bfd4e
navid hero <navid_hero_1st@yahoo.com>
2024-08-27 15:37:21 +0330

    possible fix

--- src/controllers/Client/ClientController.js
+++ src/controllers/Client/ClientController.js
@@ -151,7 +151,7 @@ exports.updateProfilePicture = async (params, avatar) => {
 		}
 
 		let newAvatarPath = `/public/uploads/avatar-${id}.png`;
-		renameFile(`/${avatar.path}`, newAvatarPath);
+		renameFile(`${__basedir}/${avatar.path}`, newAvatarPath);
 		newAvatarPath = newAvatarPath.replace("public/", "");
 
 		user = await User.findOneAndUpdate(
