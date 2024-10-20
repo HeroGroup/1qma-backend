2ec60b3d3e7b8909018f495ba47673c81b76dfde
navid hero <navid_hero_1st@yahoo.com>
2024-10-20 15:29:35 +0330

    comment askAI

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ const {
 	reconnectPlayer,
 } = require("./src/controllers/Client/GameController");
 const User = require("./src/models/User.js");
-const { askAI } = require("./src/services/openai");
+// const { askAI } = require("./src/services/openai");
 
 async function main() {
 	const whiteList = [
@@ -220,7 +220,7 @@ async function main() {
 
 	globalThis.io = io;
 
-	console.log(await askAI("what do you think of child labour?"));
+	// console.log(await askAI("what do you think of child labour?"));
 	const port = env.app.port;
 	server.listen(port, () => {
 		console.log(
